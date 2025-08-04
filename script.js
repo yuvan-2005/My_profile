3307ea5218f7dc7122f787d21989a163e1933909
yuvan-2005 <yuvanstars2005@gmail.com>
2025-08-04 14:37:56 +0530

    feat:changes

---
+++ script.js
@@ -0,0 +1,12 @@
+particlesJS("particles-js", {
+    "particles": {
+        "number": { "value": 80 },
+        "size": { "value": 3 },
+        "color": { "value": "#00ffff" },
+        "move": { "speed": 2 },
+        "line_linked": { "enable": true, "color": "#00ffff" }
+    },
+    "interactivity": {
+        "events": { "onhover": { "enable": true, "mode": "repulse" } }
+    }
+});
